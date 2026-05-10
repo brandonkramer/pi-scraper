7f48dc11ba51f0e5f370abca55babb26dc4b9cfe
brandonkramer <4xsquid@gmail.com>
2026-05-10 05:58:02 +0200

    refactor(storage): move cache.ts→cache/fetch-cache.ts, add index.ts barrel

---
+++ src/storage/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Barrel for storage sub-modules.
+ * @deprecated Prefer direct imports from storage/db/, storage/responses/, etc.
+ */
+export * from "./db.ts";
+export * from "./jobs.ts";
+export * from "./paths.ts";
+export * from "./blobs.ts";
+export * from "./context-package-results.ts";
+export * from "./cache.ts";
+export * from "./search.ts";
+export * from "./responses/store.ts";
+export * from "./responses/read.ts";
+export * from "./responses/truncate.ts";
